2eeb9be4390c13c840aa640713ae3de84b56e2d6
AlbertoJTD <albertojtd@outlook.com>
2025-04-11 10:57:43 -0600

    Add new product form and controller action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,10 @@ class ProductsController < ApplicationController
     @products = Product.all.where(visible: true).order(id: :desc)
   end
 
+  def new
+    @product = Product.new
+  end
+
   private
 
   def product_params
