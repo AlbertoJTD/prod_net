b713d232f8018c8708d5ee2819c3775740ec0b2a
AlbertoJTD <albertojtd@outlook.com>
2025-04-11 06:32:35 -0600

    Add schema information comments to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :string
+#  visible     :boolean          default(TRUE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ApplicationRecord
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :string
+#  visible     :boolean          default(TRUE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
