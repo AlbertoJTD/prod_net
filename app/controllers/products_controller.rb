9ff28b125d4465ed90a47f53bd75ec31ed8ba5c1
AlbertoJTD <albertojtd@outlook.com>
2025-04-11 20:57:22 -0600

    Display modal for product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,19 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      respond_to do |format|
+        format.html { redirect_to products_path, notice: t('.success') }
+        format.turbo_stream
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def product_params
