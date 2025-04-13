fe089157e2ea4931e9db579046d209f66eae9ccf
AlbertoJTD <albertojtd@outlook.com>
2025-04-13 01:27:36 -0600

    Add slug column to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,13 +3,17 @@
 # Table name: products
 #
 #  id          :bigint           not null, primary key
-#  name        :string
-#  description :string
+#  name        :string           not null
+#  description :string           not null
 #  visible     :boolean          default(TRUE)
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  slug        :string
 #
 class Product < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   validates :name, :description, presence: true
   validates :name, length: { minimum: 3, maximum: 200 }
 end
