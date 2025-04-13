cbe137821792a74fde4352950dbc7ff4db3a26eb
AlbertoJTD <albertojtd@outlook.com>
2025-04-13 00:07:59 -0600

    Add product detail view with error handling for not found products and localized messages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
   def index
     @products = Product.all.where(visible: true).order(id: :desc)
   end
@@ -21,7 +23,24 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
+
+  def handle_record_not_found
+    respond_to do |format|
+      format.html do
+        flash[:alert] = t('products.shared.not_found')
+        redirect_to products_path
+      end
+      format.turbo_stream do
+        flash.now[:error] = t('products.shared.not_found')
+        render_turbo_stream_flash_messages
+      end
+    end
+  end
 
   def product_params
     params.require(:product).permit(:name, :description, :visible)
