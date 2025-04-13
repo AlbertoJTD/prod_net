7cda7337cfc60107fdfd0dca016b9c93bf8e3d16
AlbertoJTD <albertojtd@outlook.com>
2025-04-13 15:10:17 -0600

    Enhance categories management with Turbo integration and improved error handling.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,10 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[ show edit update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @pagy, @categories = pagy_countless(Category.all, limit: 15)
   end
 
   # GET /categories/1 or /categories/1.json
@@ -23,27 +24,27 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
+    if @category.save
+      respond_to do |format|
+        flash.now[:success] = t('.success')
+        format.html { redirect_to categories_path, success: t('.success') }
+        format.turbo_stream
       end
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /categories/1 or /categories/1.json
   def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
+    if @category.update(category_params)
+      respond_to do |format|
+        flash.now[:success] = t('.success')
+        format.html { redirect_to categories_path, success: t('.success') }
+        format.turbo_stream
       end
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -52,19 +53,26 @@ class CategoriesController < ApplicationController
     @category.destroy!
 
     respond_to do |format|
-      format.html { redirect_to categories_path, status: :see_other, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
+      flash.now[:success] = t('.success')
+      format.html { redirect_to categories_path, success: t('.success') }
+      format.turbo_stream
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:name)
+  def handle_record_not_found
+    respond_to do |format|
+      format.html { redirect_to categories_path, status: :see_other, alert: t('categories.not_found') }
+      format.json { head :no_content }
     end
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
