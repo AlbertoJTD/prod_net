4be3a286bb0ba714e0493c3dc00aa0e86b484c9b
AlbertoJTD <albertojtd@outlook.com>
2025-04-13 22:31:16 -0600

    Enhance Shared::ModalComponent to accept customizable data attributes

--- app/components/shared/modal_component.rb
+++ app/components/shared/modal_component.rb
@@ -8,5 +8,6 @@ class Shared::ModalComponent < ApplicationComponent
 
   def initialize(**options)
     @title = options[:title]
+    @data_modal = options[:data_modal] || { controller: 'shared--modal', shared__modal_target: 'modal', action: 'turbo:frame-load->shared--modal#open turbo:submit-end->shared--modal#hideModal' }
   end
 end
