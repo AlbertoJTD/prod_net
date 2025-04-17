bf9b086ea05248e72f55e3f39ecb2faa6ca64e9c
AlbertoJTD <albertojtd@outlook.com>
2025-04-17 13:03:16 -0600

    Add test for comments controller

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,26 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  pending "WIP 🚧 #{__FILE__}"
+  let(:comment) { create(:comment) }
 
-  # describe "GET /edit" do
-  #   it "returns http success" do
-  #     get "/comments/edit"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe 'DELETE /destroy' do
+    context 'request as html' do
+      it 'returns http success' do
+        delete comment_path(comment)
+        expect(response).to have_http_status(:redirect)
+      end
 
-  # describe "GET /update" do
-  #   it "returns http success" do
-  #     get "/comments/update"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+      it 'redirects to the product' do
+        delete comment_path(comment)
+        expect(response).to redirect_to(product_path(comment.comentable))
+      end
 
-  # describe "GET /destroy" do
-  #   it "returns http success" do
-  #     get "/comments/destroy"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+      it 'should decrease the comment count' do
+        comment = create(:comment)
+
+        expect {
+          delete comment_path(comment)
+        }.to change(Comment, :count).by(-1)
+      end
+    end
+
+    context 'request as turbo_stream' do
+      it 'assings the comment to @comment' do
+        delete comment_path(comment), as: :turbo_stream
+        expect(assigns(:comment)).to eq(comment)
+      end
+
+      it "returns http success" do
+        delete comment_path(comment), as: :turbo_stream
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
 end
